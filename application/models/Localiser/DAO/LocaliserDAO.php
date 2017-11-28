672019e6d6c1b4c18dd514755c30cd9d052371db
nyandams <nyandams34@gmail.com>
2017-11-28 23:46:29 +0100

    implémentation localiser

---
+++ application/models/Localiser/DAO/LocaliserDAO.php
@@ -0,0 +1,33 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class LocaliserDAO extends CI_Model
+{
+    private $correlationTable = array(
+        'idZone'            => 'idZone',
+        'idReservation'     => 'idReservation',
+        'nbEmplacement'     => 'nbEmplacement'
+    );
+    
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('Localiser/NotFoundLocaliserException');
+        $this->load->model('Localiser/DTO/LocaliserDTO');
+        $this->load->model('Localiser/DTO/LocaliserCollection');
+    }
+    
+    
+    
+    /**
+     * passage d'un tableau récupéré en BDD à un dto
+     * @param $db
+     * @return LocaliserDTO
+     */
+    private function hydrateFromDatabase($db){
+        $dto = new LocaliserDTO();
+        foreach($this->correlationTable as $setterName => $getterName){
+            $setter = 'set' .ucwords($setterName);
+            $dto->$setter($db->$getterName);
+        }
+        return $dto;
+    }
+}
