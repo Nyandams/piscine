cae48289c26190f72e0dca4ef797b7f01d56a97d
Alia <aliachawaf@gmail.com>
2017-12-25 14:11:31 +0100

    TypeJeuDAO

--- application/models/TypeJeu/DAO/TypeJeuDAO.php
+++ application/models/TypeJeu/DAO/TypeJeuDAO.php
@@ -15,7 +15,104 @@ class TypeJeuDAO extends CI_Model
         $this->load->model('TypeJeu/DTO/TypeJeuCollection');
     }
     
+
+ 
+    /**
+     * renvoie une collection d' "TypeJeuDTO"
+     * @return TypeJeuCollection
+     */
+    public function getTypeJeux(){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->get()
+                             ->result();
+        
+        $typejeuCollection = new TypeJeuCollection();
+        
+        foreach($resultat as $element){
+            $dto = $this->hydrateFromDatabase($element);
+            $typejeuCollection->append($dto);
+        }
+        
+        return $typejeuCollection;
+    }
     
+    /**
+     * sauvegarde un type de jeu dans la BDD
+     * @param TypeJeuDTO $typejeuDTO
+     */
+    public function saveTypeJeu($typejeuDTO){
+        $bdd = hydrateFromDTO($typejeuDTO);
+        $this->db->set($bdd)
+                 ->insert($this->table);
+    }
+    
+    /**
+     * Supprime le typejeuDTO de la BDD
+     * @param TypeJeuDTO $typejeuDTO
+     * @return Boolean
+     */
+    public function deleteTypeJeu($typejeuDTO){
+        $id = $typejeuDTO->getIdTypeJeu();
+        return $this->db->where('id', $id)->delete($this->table);
+    }
+    
+    
+    public function updateTypeJeu($dto){
+        $bdd = hydrateFromDTO($dto);
+        
+        $this->db->update_batch($this->table, $bdd, 'idTypeJeu');
+    }
+    
+    /**
+     * @param int $id
+     * @return EditeurDTO
+     */
+    public function getTypeJeuById($id){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->where('idTypeJeu', $id)
+                             ->get();
+        
+        $dto = hydrateFromDatabase($resultat);
+        return $dto;
+    }
+    
+    /**
+     * retourne un typejeuCollection contenant les types de jeu pouvant correspondre à $chaineCar
+     * @param string $chaineCar
+     * @return TypeJeuCollection
+     */
+    public function listeRechercheTypeJeu($chaineCar){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->like('libelleTypeJeu', $chaineCar)
+                             ->get()
+                             ->result();
+        
+        $typejeuCollection = new TypeJeuCollection();
+        
+        foreach($resultat as $element){
+            $dto = $this->hydrateFromDatabase($element);
+            $TypeJeuCollection->append($dto);
+        }
+        
+        return $typejeuCollection;
+    }
+    
+     
+    /**
+     * @param TypeJeuDTO $dto
+     * @return array('id' => value)
+     */
+    private function hydrateFromDTO($dto){
+        $bdd = array();
+        foreach($this->correlationTable as $getterName => $setterName){
+            $getter = 'get'.ucwords($getterName);
+            $bdd[$setterName] = $dto->$getter();
+        }
+        return $bdd;
+    }
     
     /**
      * passage d'un tableau récupéré en BDD à un dto
@@ -31,3 +128,4 @@ class TypeJeuDAO extends CI_Model
         return $dto;
     }
 }
+
