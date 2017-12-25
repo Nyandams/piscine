6d2a1d1ea8fcf0aff2af158ca9005953d2201937
Alia <aliachawaf@gmail.com>
2017-12-25 15:16:49 +0100

    FactureDAO

--- application/models/Facture/DAO/FactureDAO.php
+++ application/models/Facture/DAO/FactureDAO.php
@@ -16,8 +16,107 @@ class FactureDAO extends CI_Model
         $this->load->model('Facture/DTO/FactureCollection');
     }
     
+   
+
     
- 
+    /**
+     * renvoie une collection d' "FactureDTO"
+     * @return FactureCollection
+     */
+    public function getFactures(){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->get()
+                             ->result();
+        
+        $factureCollection = new FactureCollection();
+        
+        foreach($resultat as $element){
+            $dto = $this->hydrateFromDatabase($element);
+            $factureCollection->append($dto);
+        }
+        
+        return $factureCollection;
+    }
+    
+    /**
+     * sauvegarde une facture dans la BDD
+     * @param FactureDTO $factureDTO
+     */
+    public function saveFacture($factureDTO){
+        $bdd = hydrateFromDTO($factureDTO);
+        $this->db->set($bdd)
+                 ->insert($this->table);
+    }
+    
+    /**
+     * Supprime le factureDTO de la BDD
+     * @param FactureDTO $factureDTO
+     * @return Boolean
+     */
+    public function deleteFacture($factureDTO){
+        $id = $factureDTO->getIdFacture();
+        return $this->db->where('id', $id)->delete($this->table);
+    }
+    
+    
+    public function updateFacture($dto){
+        $bdd = hydrateFromDTO($dto);
+        
+        $this->db->update_batch($this->table, $bdd, 'idFacture');
+    }
+    
+    /**
+     * @param int $id
+     * @return FactureDTO
+     */
+    public function getFactureById($id){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->where('idFacture', $id)
+                             ->get();
+        
+        $dto = hydrateFromDatabase($resultat);
+        return $dto;
+    }
+    
+    /**
+     * retourne une factureCollection contenant les factures pouvant correspondre à $chaineCar (recherche par date d'émission de la facture)
+     * @param string $chaineCar
+     * @return FactureCollection
+     */
+    public function listeRechercheFacture($chaineCar){
+        $resultat = $this->db->select()
+                             ->from($this->table)
+                             ->like('dateEmissionFacture', $chaineCar)
+                             ->get()
+                             ->result();
+        
+        $factureCollection = new FactureCollection();
+        
+        foreach($resultat as $element){
+            $dto = $this->hydrateFromDatabase($element);
+            $FactureCollection->append($dto);
+        }
+        
+        return $factureCollection;
+    }
+    
+
+
+    /**
+     * @param FactureDTO $dto
+     * @return array('id' => value)
+     */
+    private function hydrateFromDTO($dto){
+        $bdd = array();
+        foreach($this->correlationTable as $getterName => $setterName){
+            $getter = 'get'.ucwords($getterName);
+            $bdd[$setterName] = $dto->$getter();
+        }
+        return $bdd;
+    }
+    
     /**
      * passage d'un tableau récupéré en BDD à un dto
      * @param $db
