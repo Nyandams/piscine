c7611b3aded711e61a2c9dbe1cddfa06dd78b0f0
nyandams <nyandams34@gmail.com>
2017-12-29 20:06:21 +0100

    service pour récupérer tout ce qui est lié à une réservation (sans localiser et zone, je le traite après)

--- application/models/EnsembleReservation/DTO/EnsembleReservationDTO.php
+++ application/models/EnsembleReservation/DTO/EnsembleReservationDTO.php
@@ -7,4 +7,106 @@ class EnsembleReservationDTO extends CI_Model
      */
     private $ensembleReserverCollection = null;
     
+    /**
+     * @var int
+     */
+    private $idReservation = null;
+    
+    /**
+     * @var float
+     */
+    private $prixNegociationReservation = null;
+    
+    /**
+     * @var int
+     */
+    private $idFestival = null;
+    
+    /***
+     * @var int
+     */
+    private $idEditeur = null;
+    
+    
+    
+    /**
+     * @return the $ensembleReserverCollection
+     */
+    public function getEnsembleReserverCollection()
+    {
+        return $this->ensembleReserverCollection;
+    }
+
+    /**
+     * @return the $idReservation
+     */
+    public function getIdReservation()
+    {
+        return $this->idReservation;
+    }
+
+    /**
+     * @return the $prixNegociationReservation
+     */
+    public function getPrixNegociationReservation()
+    {
+        return $this->prixNegociationReservation;
+    }
+
+    /**
+     * @return the $idFestival
+     */
+    public function getIdFestival()
+    {
+        return $this->idFestival;
+    }
+
+    /**
+     * @return the $idEditeur
+     */
+    public function getIdEditeur()
+    {
+        return $this->idEditeur;
+    }
+
+    /**
+     * @param EnsembleReserverCollection $ensembleReserverCollection
+     */
+    public function setEnsembleReserverCollection($ensembleReserverCollection)
+    {
+        $this->ensembleReserverCollection = $ensembleReserverCollection;
+    }
+
+    /**
+     * @param number $idReservation
+     */
+    public function setIdReservation($idReservation)
+    {
+        $this->idReservation = $idReservation;
+    }
+
+    /**
+     * @param number $prixNegociationReservation
+     */
+    public function setPrixNegociationReservation($prixNegociationReservation)
+    {
+        $this->prixNegociationReservation = $prixNegociationReservation;
+    }
+
+    /**
+     * @param number $idFestival
+     */
+    public function setIdFestival($idFestival)
+    {
+        $this->idFestival = $idFestival;
+    }
+
+    /**
+     * @param number $idEditeur
+     */
+    public function setIdEditeur($idEditeur)
+    {
+        $this->idEditeur = $idEditeur;
+    }
+
 }
--- application/models/EnsembleReservation/EnsembleReservationFactory.php
+++ application/models/EnsembleReservation/EnsembleReservationFactory.php
@@ -1,1 +1,18 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class EnsembleReservationFactory extends CI_Model
+{
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Reservation/ReservationFactory');
+        $this->load->model('EnsembleReservation/EnsembleReserverFactory');
+    }
+    
+    
+    static public function getInstance() {
+        $reserverService = EnsembleReserverFactory::getInstance();
+        $reservationDao  = ReservationFactory::getInstance();
+        $dao = new EnsembleReservationService();
+        return $dao->initConstruct($reserverService, $reservationDao);
+    }
+}
