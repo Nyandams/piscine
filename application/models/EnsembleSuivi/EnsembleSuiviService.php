4f9414fae8aeb52ec3485dca5c0b6afc06e3a559
aliachawaf <alia.chawaf@igtp3-mac14.isim.intra>
2018-01-12 18:57:34 +0100

    ensemble des suivis

--- application/models/EnsembleSuivi/EnsembleSuiviService.php
+++ application/models/EnsembleSuivi/EnsembleSuiviService.php
@@ -53,7 +53,7 @@ class EnsembleSuiviService extends CI_Model
 
 
 
-    #renvoie que les suivis des éditeurs pas contacté
+/* renvoie que les suivis des éditeurs pas contacté */
 
    public function getSuiviNonContacteDTOByIdFestival ($idFestival) {
         $ensembleSuiviCollection = new EnsembleSuiviCollection();
@@ -80,4 +80,147 @@ class EnsembleSuiviService extends CI_Model
         
     }
     
+
+
+/* renvoie que les suivis des éditeurs contactés 1 fois mais qui n'ont pas répondu */
+
+   public function getSuivi1erContactSansReponseDTOByIdFestival ($idFestival) {
+        $ensembleSuiviCollection = new EnsembleSuiviCollection();
+        $suivisDTO = $this->suiviDAO->getSuiviByIdFestival($idFestival);
+
+        foreach ($suivisDTO as $key => $suiviDTO) {
+            $ensembleSuiviTmp = new EnsembleSuiviDTO();
+            
+            $premierContact = $suiviDTO->getPremierContact();
+            $reponseEditeur = $suiviDTO->getReponseEditeur();
+
+            if ($premierContact!=null and $reponseEditeur== -1) {
+
+                $editeurContactDTO = $this->editeurContactDAO->getEditeurContactByIdEditeur($suiviDTO->getIdEditeur());
+            
+                $ensembleSuiviTmp->setSuiviDTO($suiviDTO);
+                $ensembleSuiviTmp->setEditeurContactDTO($editeurContactDTO);
+            
+                $ensembleSuiviCollection->append($ensembleSuiviTmp);
+
+            }
+        }
+
+        return $ensembleSuiviCollection;
+        
+    }
+
+
+/* renvoie que les suivis des éditeurs contactés 2 fois mais qui n'ont pas répondu */
+
+   public function getSuivi2emeContactSansReponseDTOByIdFestival ($idFestival) {
+        $ensembleSuiviCollection = new EnsembleSuiviCollection();
+        $suivisDTO = $this->suiviDAO->getSuiviByIdFestival($idFestival);
+
+        foreach ($suivisDTO as $key => $suiviDTO) {
+            $ensembleSuiviTmp = new EnsembleSuiviDTO();
+            
+            $secondContact = $suiviDTO->getSecondContact();
+            $reponseEditeur = $suiviDTO->getReponseEditeur();
+
+            if ($secondContact!=null and $reponseEditeur== -1) {
+
+                $editeurContactDTO = $this->editeurContactDAO->getEditeurContactByIdEditeur($suiviDTO->getIdEditeur());
+            
+                $ensembleSuiviTmp->setSuiviDTO($suiviDTO);
+                $ensembleSuiviTmp->setEditeurContactDTO($editeurContactDTO);
+            
+                $ensembleSuiviCollection->append($ensembleSuiviTmp);
+
+            }
+        }
+
+        return $ensembleSuiviCollection;
+        
+    }
+
+
+
+/* renvoie que les suivis des éditeurs qui ont répondu OUI (au 1er ou 2eme contact) */
+
+   public function getSuiviReponseOuiDTOByIdFestival ($idFestival) {
+        $ensembleSuiviCollection = new EnsembleSuiviCollection();
+        $suivisDTO = $this->suiviDAO->getSuiviByIdFestival($idFestival);
+
+        foreach ($suivisDTO as $key => $suiviDTO) {
+            $ensembleSuiviTmp = new EnsembleSuiviDTO();
+            
+            $reponseEditeur = $suiviDTO->getReponseEditeur();
+
+            if ($reponseEditeur==3) {
+
+                $editeurContactDTO = $this->editeurContactDAO->getEditeurContactByIdEditeur($suiviDTO->getIdEditeur());
+            
+                $ensembleSuiviTmp->setSuiviDTO($suiviDTO);
+                $ensembleSuiviTmp->setEditeurContactDTO($editeurContactDTO);
+            
+                $ensembleSuiviCollection->append($ensembleSuiviTmp);
+
+            }
+        }
+
+        return $ensembleSuiviCollection;
+        
+    }
+
+
+/* renvoie que les suivis des éditeurs qui ont répondu NON (au 1er ou 2eme contact) */
+
+   public function getSuiviReponseNonDTOByIdFestival ($idFestival) {
+        $ensembleSuiviCollection = new EnsembleSuiviCollection();
+        $suivisDTO = $this->suiviDAO->getSuiviByIdFestival($idFestival);
+
+        foreach ($suivisDTO as $key => $suiviDTO) {
+            $ensembleSuiviTmp = new EnsembleSuiviDTO();
+            
+            $reponseEditeur = $suiviDTO->getReponseEditeur();
+
+            if ($reponseEditeur==1) {
+
+                $editeurContactDTO = $this->editeurContactDAO->getEditeurContactByIdEditeur($suiviDTO->getIdEditeur());
+            
+                $ensembleSuiviTmp->setSuiviDTO($suiviDTO);
+                $ensembleSuiviTmp->setEditeurContactDTO($editeurContactDTO);
+            
+                $ensembleSuiviCollection->append($ensembleSuiviTmp);
+
+            }
+        }
+
+        return $ensembleSuiviCollection;
+        
+    }
+
+
+/* renvoie que les suivis des éditeurs qui ont répondu PEUT ETRE (au 1er ou 2eme contact) */
+
+   public function getSuiviReponsePeutEtreDTOByIdFestival ($idFestival) {
+        $ensembleSuiviCollection = new EnsembleSuiviCollection();
+        $suivisDTO = $this->suiviDAO->getSuiviByIdFestival($idFestival);
+
+        foreach ($suivisDTO as $key => $suiviDTO) {
+            $ensembleSuiviTmp = new EnsembleSuiviDTO();
+            
+            $reponseEditeur = $suiviDTO->getReponseEditeur();
+
+            if ($reponseEditeur==2) {
+
+                $editeurContactDTO = $this->editeurContactDAO->getEditeurContactByIdEditeur($suiviDTO->getIdEditeur());
+            
+                $ensembleSuiviTmp->setSuiviDTO($suiviDTO);
+                $ensembleSuiviTmp->setEditeurContactDTO($editeurContactDTO);
+            
+                $ensembleSuiviCollection->append($ensembleSuiviTmp);
+
+            }
+        }
+
+        return $ensembleSuiviCollection;
+        
+    }
 }
